0a256e038133db0db1364d39b1cbb086196c2234
fikrirmdhna <fikrirmdhna01@gmail.com>
2023-09-13 11:03:48 +0700

    add a test

--- main/tests.py
+++ main/tests.py
@@ -9,3 +9,7 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+
+    def test_main_has_utf8_charset(self):
+        response = Client().get('/main/')
+        self.assertIn('utf-8', response.get('Content-Type', '').lower())
