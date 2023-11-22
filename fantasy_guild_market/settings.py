a91fcb6104baac0b0ab20c1a4715bb6e5c273c54
fikrirmdhna <fikrirmdhna01@gmail.com>
2023-11-22 11:26:28 +0700

    add csrf trusted origin

--- fantasy_guild_market/settings.py
+++ fantasy_guild_market/settings.py
@@ -146,3 +146,4 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
+CSRF_TRUSTED_ORIGINS = ['https://fikri-dhiya21-tugas.pbp.cs.ui.ac.id']
